fc2e51001022f87179fd36d390d489603dd70c27
leonard <leonard.tschora@bcmenergy.fr>
2023-03-21 11:05:07 +0100

    svr

--- scripts/svr.py
+++ scripts/svr.py
@@ -4,12 +4,12 @@ from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
 from src.models.spliter import MySpliter
-from src.models.svr import MultiSVRWrapper, ChainSVRWrapper
+from src.models.svr_wrapper import MultiSVRWrapper, ChainSVRWrapper
 from src.models.parallel_scikit import set_all_seeds
 
 ############## Construct model wrapper and load data
 spliter = MySpliter(365, shuffle=False)
-model_wrapper = MultiSVR("TEST", "Lyon", spliter=spliter)
+model_wrapper = MultiSVRWrapper("TEST", "Lyon", spliter=spliter)
 X, Y = model_wrapper.load_train_dataset()
 
 ############## Set some params
